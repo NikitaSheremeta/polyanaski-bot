d4d9575ab800537bf734246f7ee7c16b6666293a
sheremeta <sheremeta@hub.sknt.ru>
2020-09-09 19:06:04 +0300

    v.1.0.1: Code refactoring

--- src/util/keyboards.js
+++ src/util/keyboards.js
@@ -4,18 +4,12 @@ const getMenuKeyboard = (ctx) => {
   const one = ctx.i18n.t('keyboards.menu.one');
   const two = ctx.i18n.t('keyboards.menu.two');
   const three = ctx.i18n.t('keyboards.menu.three');
-  const four = ctx.i18n.t('keyboards.menu.four');
-  const five = ctx.i18n.t('keyboards.menu.five');
-  const six = ctx.i18n.t('keyboards.menu.six');
 
-  let menuKeyboard = Markup.keyboard([
-    [one, two, three],
-    [four, five, six]
-  ]);
+  let menuKeyboard = Markup.keyboard([one, two, three]);
 
   menuKeyboard = menuKeyboard.resize().extra();
 
   return { menuKeyboard };
 };
 
-module.exports.getMenuKeyboard = getMenuKeyboard;
+module.exports = { getMenuKeyboard };
