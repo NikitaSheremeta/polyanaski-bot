ce24ac2bbd280916a0c8672e671e4d2ed5e226be
sheremeta <sheremeta@hub.sknt.ru>
2020-09-11 17:06:59 +0300

    v.1.0.1: Development of the start scene

--- src/controllers/start/index.js
+++ src/controllers/start/index.js
@@ -1,14 +1,12 @@
-const Stage = require('telegraf/stage');
+const { Extra, Markup } = require('telegraf');
 const Scene = require('telegraf/scenes/base');
 
-const { getMainMenu } = require('../../util/keyboards');
-
-const { leave } = Stage;
+const { getStartMenu } = require('../../util/keyboards');
 
 const start = new Scene('start');
 
 start.enter((ctx) => {
-  const { mainMenu } = getMainMenu(ctx);
+  const { startMenu } = getStartMenu(ctx);
 
   const messages = {
     greeting: ctx.i18n.t('scenes.start.greeting'),
@@ -17,11 +15,14 @@ start.enter((ctx) => {
 
   ctx.reply(messages.greeting);
 
-  ctx.reply(messages.description, mainMenu);
+  ctx.reply(messages.description, startMenu);
 });
 
-start.leave((ctx) => ctx.reply('Bye!'));
+start.leave((ctx) => {
+  ctx.reply('Выход из сценария', Extra.markup(Markup.removeKeyboard()));
+});
 
-start.hears(/hi/gi, leave());
+start.hears(/инфо/gi, (ctx) => ctx.reply('Стартовая сцена'));
+start.hears(/понятно/gi, (ctx) => ctx.scene.leave(ctx));
 
 module.exports = { start };
