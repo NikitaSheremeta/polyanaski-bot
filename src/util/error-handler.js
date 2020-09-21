a8905d8ee9ac17108bb820dc2f3f37374e285b7b
sheremeta <sheremeta@hub.sknt.ru>
2020-09-21 15:23:15 +0300

    v.1.0.1: Added error handler file

---
+++ src/util/error-handler.js
@@ -0,0 +1,15 @@
+const logger = require('./logger');
+
+const asyncWrapper = (fn) => {
+  return async function(ctx, next) {
+    try {
+      return await fn(ctx);
+    } catch (error) {
+      logger.error(ctx, 'asyncWrapper error, %O', error);
+      await ctx.reply(ctx.i18n.t('shared.error'));
+      return next();
+    }
+  };
+};
+
+module.exports = { asyncWrapper };
