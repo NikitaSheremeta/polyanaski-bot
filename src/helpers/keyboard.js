323c6a266f8422b1c1e25f77e72ed2d5ce63b96b
sheremeta <sheremeta@hub.sknt.ru>
2020-09-24 13:30:41 +0300

    v.1.0.1: Added buttons class

--- src/helpers/keyboard.js
+++ src/helpers/keyboard.js
@@ -1,24 +1,11 @@
 const Extra = require('telegraf/extra');
 const Markup = require('telegraf/markup');
+const Buttons = require('./buttons');
 
-class Keyboard {
+class Keyboard extends Buttons {
   constructor(ctx, markdown = false) {
-    this.ctx = ctx;
+    super(ctx);
     this.markdown = markdown;
-    this.buttons = {
-      launch: this.ctx.i18n.t('shared.launch'),
-      back: this.ctx.i18n.t('navigation.back'),
-      next: this.ctx.i18n.t('navigation.next'),
-      skipass: this.ctx.i18n.t('categories.ski-passes'),
-      rent: this.ctx.i18n.t('categories.rent'),
-      instructors: this.ctx.i18n.t('categories.instructors'),
-      freeride: this.ctx.i18n.t('categories.freeride'),
-      contacts: this.ctx.i18n.t('categories.contacts'),
-      rosaKhutor: this.ctx.i18n.t('resorts.rosa-khutor'),
-      gorkiGorod: this.ctx.i18n.t('resorts.gorki-gorod'),
-      gazprom: this.ctx.i18n.t('resorts.gazprom'),
-      alpikaService: this.ctx.i18n.t('resorts.alpika-service'),
-    };
   }
 
   keyboard(layout) {
