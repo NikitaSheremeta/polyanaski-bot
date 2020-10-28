3c633ac4f9d88a8ee33b75c22b97c27bbfd6b026
sheremeta <sheremeta@hub.sknt.ru>
2020-10-28 14:08:53 +0300

    v.1.0.1: Added asyncWrapper and some code refactoring in the childrens school controller

--- src/controllers/childrens-school.js
+++ src/controllers/childrens-school.js
@@ -1,14 +1,15 @@
 const Stage = require('telegraf/stage');
 const Scene = require('telegraf/scenes/base');
-// const Extra = require('telegraf/extra');
+const Extra = require('telegraf/extra');
 const { match } = require('telegraf-i18n');
 const Messages = require('../helpers/messages');
 const Keyboards = require('../helpers/keyboards');
 const { logger } = require('../util/logger');
+const { asyncWrapper } = require('../util/error-handler');
 
 const { leave } = Stage;
 const scene = new Scene('childrens-school');
-// const markup = Extra.markdown();
+const markup = Extra.markdown();
 
 scene.enter(async (ctx) => {
   logger.debug(ctx, 'Enters the childrens school scene');
@@ -33,17 +34,21 @@ scene.leave(async (ctx) => {
   await ctx.reply(messages.mainMenu, keyboards.main);
 });
 
-scene.hears(match('resorts.krasnayaPolyana'), async (ctx) => {
-  const messages = new Messages(ctx);
+scene.hears(
+  match('resorts.krasnayaPolyana'),
+  asyncWrapper(async (ctx) => await ctx.reply(
+    '[Детская школа в Красной поляне](https://telegra.ph/Detskaya-shkola-v-Krasnoj-polyane-10-28)',
+    markup
+  ))
+);
 
-  await ctx.reply(messages.workInProgress);
-});
-
-scene.hears(match('resorts.rosaKhutor'), async (ctx) => {
-  const messages = new Messages(ctx);
-
-  await ctx.reply(messages.workInProgress);
-});
+scene.hears(
+  match('resorts.rosaKhutor'),
+  asyncWrapper(async (ctx) => await ctx.reply(
+    '[Детская школа на Роза хутор](https://telegra.ph/Detskaya-shkola-na-Roza-hutor-10-28)',
+    markup
+  ))
+);
 
 scene.hears(match('navigation.back'), leave());
 
