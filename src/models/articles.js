7338411a1de1b9d23d49bc97d6c950e5dd5f2f28
sheremeta <sheremeta@hub.sknt.ru>
2020-11-08 00:26:00 +0300

    v.1.0.1: Added collection object to schema class in articles model

--- src/models/articles.js
+++ src/models/articles.js
@@ -1,8 +1,13 @@
 const { Schema, model} = require('mongoose');
 
-const schema = new Schema({
-  title: { type: String, required: true },
-  link: { type: String, required: true }
-});
+const schema = new Schema(
+  {
+    title: { type: String, required: true },
+    link: { type: String, required: true }
+  },
+  {
+    collection : 'articles'
+  }
+);
 
 module.exports = model('Articles', schema);
