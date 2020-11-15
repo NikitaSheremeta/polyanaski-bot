9dce7dcb5483a8b3031eb50b651ea340c7e247f1
sheremeta <sheremeta@hub.sknt.ru>
2020-11-15 21:18:11 +0300

    Fixed bug with asynchronous start scene

--- src/controllers/start.js
+++ src/controllers/start.js
@@ -1,6 +1,4 @@
-const Stage = require('telegraf/stage');
 const Scene = require('telegraf/scenes/base');
-const { match } = require('telegraf-i18n');
 
 const Keyboards = require('../helpers/keyboards');
 const Messages = require('../helpers/messages');
@@ -9,57 +7,46 @@ const { logger } = require('../util/logger');
 
 const Users = require('../models/users');
 
-const { leave } = Stage;
 const scene = new Scene('start');
 
 scene.enter(async (ctx) => {
   logger.debug(ctx, 'Enters the start scene');
 
-  const keyboards = new Keyboards(ctx);
   const messages = new Messages(ctx);
 
-  /**
-   * Arguing on the topic, does it make sense to leave the authorization code
-   * in the controller or still move it to the new "actions" directory.
-   */
   const userID = String(ctx.from.id);
 
   const user = await Users.findById(userID);
 
   if (user) {
-    logger.debug(ctx, messages.welcomeBack);
-  } else {
-    const MESSAGE_INTERVAL = 1600;
+    logger.debug(ctx, 'Welcome back!');
 
-    const userData = {
-      _id: userID,
-      created: new Date().getTime(),
-      username: ctx.from.username,
-      name: `${ctx.from.first_name} ${ctx.from.last_name}`,
-      language: ctx.i18n.languageCode
-    };
+    await ctx.reply(messages.welcomeBack);
 
-    const newUser = new Users(userData);
-
-    try {
-
-      await newUser.save().then(() => {
-        logger.debug(ctx, 'New user has been created');
-      });
-
-    } catch (error) {
-
-      logger.debug(ctx, 'New user has NOT been created');
-
-    }
-
-    await ctx.reply(messages.greeting).then(() => {
-      setTimeout(() => ctx.reply(
-        messages.description,
-        keyboards.launch
-      ), MESSAGE_INTERVAL);
-    });
+    return await ctx.scene.leave();
   }
+
+  const userData = {
+    _id: userID,
+    created: new Date().getTime(),
+    username: ctx.from.username,
+    name: `${ctx.from.first_name} ${ctx.from.last_name}`,
+    language: ctx.i18n.languageCode
+  };
+
+  const newUser = new Users(userData);
+
+  try {
+    await newUser.save().then(() => {
+      logger.debug(ctx, 'New user has been created');
+    });
+  } catch (error) {
+    logger.debug(ctx, 'New user has NOT been created');
+  }
+
+  await ctx.reply(messages.greeting);
+
+  return await ctx.scene.leave();
 });
 
 scene.leave(async (ctx) => {
@@ -68,9 +55,7 @@ scene.leave(async (ctx) => {
   const keyboards = new Keyboards(ctx);
   const messages = new Messages(ctx);
 
-  await ctx.reply(messages.go, keyboards.main);
+  return await ctx.reply(messages.description, keyboards.main);
 });
-
-scene.hears(match('shared.launch'), leave());
 
 module.exports = { start: scene };
