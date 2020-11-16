82db030cd663af3d8a3b10ee63266a8c8f82bcf2
sheremeta <sheremeta@hub.sknt.ru>
2020-11-16 15:50:46 +0300

    The consultation scene is over

--- src/controllers/consultation.js
+++ src/controllers/consultation.js
@@ -1,33 +1,26 @@
-const Stage = require('telegraf/stage');
 const Scene = require('telegraf/scenes/base');
-const { match } = require('telegraf-i18n');
 
-const Keyboards = require('../helpers/keyboards');
 const Messages = require('../helpers/messages');
+const InlineKeyboards = require('../helpers/inline-keyboards');
 
 const { logger } = require('../util/logger');
 
-const { leave } = Stage;
 const scene = new Scene('consultation');
 
 scene.enter(async (ctx) => {
   logger.debug(ctx, 'Enters the consultation scene');
 
-  const keyboards = new Keyboards(ctx);
   const messages = new Messages(ctx);
+  const inlineKeyboards = new InlineKeyboards(ctx);
 
-  await ctx.reply(messages.workInProgress, keyboards.back);
+  await ctx.reply(messages.consultation, inlineKeyboards.consultation);
+
+  return await ctx.scene.leave();
 });
 
 scene.leave(async (ctx) => {
   logger.debug(ctx, 'Leaves the consultation scene');
-
-  const keyboards = new Keyboards(ctx);
-  const messages = new Messages(ctx);
-
-  await ctx.reply(messages.mainMenu, keyboards.main);
 });
 
-scene.hears(match('navigation.back'), leave());
 
 module.exports = { consultation: scene };
