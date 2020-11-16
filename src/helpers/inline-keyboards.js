9f374d08702b6dbcc5aef18ce9f48c52f026f27e
sheremeta <sheremeta@hub.sknt.ru>
2020-11-16 23:38:55 +0300

    v.1.0.1: Added booking method to the inline keyboards helper

--- src/helpers/inline-keyboards.js
+++ src/helpers/inline-keyboards.js
@@ -6,26 +6,52 @@ const Buttons = require('./buttons');
 class InlineKeyboards extends Buttons {
   constructor(ctx) {
     super(ctx);
+    this.markdown = false;
+  }
+
+  // Set extra markdown for the buttons
+  set extraMarkdown(isMarkdown) {
+    this.markdown = isMarkdown;
   }
 
   // Result keyboard constructor
   keyboard(layout) {
     const keyboard = Markup.inlineKeyboard(layout);
 
+    if (this.markdown) {
+      return Extra.markdown().markup(keyboard);
+    }
+
     return Extra.markup(keyboard);
   }
 
+  // Consultation inline keyboard
   get consultation() {
     const layout = [
       [{
         text: this.buttons.consultationWithInstructor,
-        url: 't.me/@EvgenyVolkov1'
+        url: this.buttons.contactInstructor
       }],
       [{
         text: this.buttons.consultationWithSupport,
-        url: 't.me/@NikitaSheremeta'
+        url: this.buttons.contactTechSupport
       }]
     ];
+
+    return this.keyboard(layout);
+  }
+
+  // Booking inline keyboard
+  booking(text, url) {
+    if (!text) {
+      text = this.buttons.booking;
+    }
+
+    if (!url) {
+      url = this.buttons.contactInstructor;
+    }
+
+    const layout = [ [{ text, url }] ];
 
     return this.keyboard(layout);
   }
