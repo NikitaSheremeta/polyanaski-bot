7d02a4fa513afa80cfe585ea0fbd5a46f668c4fa
sheremeta <sheremeta@hub.sknt.ru>
2020-11-25 17:25:58 +0300

    v.1.0.1: Added download map button

--- src/helpers/buttons.js
+++ src/helpers/buttons.js
@@ -8,9 +8,9 @@ class Buttons {
       childrensSchool: this.ctx.i18n.t('categories.childrensSchool'),
       consultation: this.ctx.i18n.t('categories.consultation'),
       consultationWithInstructor: this.ctx.i18n.t('scenes.consultation.consultationWithInstructor'),
-      techSupport: this.ctx.i18n.t('scenes.consultation.techSupport'),
       contactInstructor: this.ctx.i18n.t('contacts.instructor'),
       contactTechSupport: this.ctx.i18n.t('contacts.techSupport'),
+      downloadMap: this.ctx.i18n.t('util.downloadMap'),
       forADay: this.ctx.i18n.t('scenes.weather.forADay'),
       forAWeak: this.ctx.i18n.t('scenes.weather.forAWeak'),
       freeride: this.ctx.i18n.t('categories.freeride'),
@@ -26,6 +26,7 @@ class Buttons {
       rosaKhutor: this.ctx.i18n.t('resorts.rosaKhutor'),
       singleSkiPass: this.ctx.i18n.t('util.singleSkiPass'),
       skiPasses: this.ctx.i18n.t('categories.skiPasses'),
+      techSupport: this.ctx.i18n.t('scenes.consultation.techSupport'),
       trailMaps: this.ctx.i18n.t('categories.trailMaps'),
       weather: this.ctx.i18n.t('categories.weather')
     };
