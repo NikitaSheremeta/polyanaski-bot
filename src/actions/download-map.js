1e4d1a0f31c081bdc123853ca990238c2ef842d7
sheremeta <sheremeta@hub.sknt.ru>
2020-11-26 15:51:21 +0300

    v.1.0.1: Added download map action

---
+++ src/actions/download-map.js
@@ -0,0 +1,55 @@
+const path = require('path');
+
+const { logger } = require('../util/logger');
+
+const IMAGES_DIR = path.join(__dirname, '..', 'assets', 'images');
+
+const TRAIL_MAPS = {
+  mainMap: {
+    filename: 'main-map.jpg',
+    source: path.join(IMAGES_DIR, 'main-map.jpg'),
+    sourcePreview: path.join(IMAGES_DIR, 'main-map-preview.jpg')
+  },
+  krasnayaPolyana: {
+    filename: 'krasnaya-polyana-map.jpg',
+    source: path.join(IMAGES_DIR, 'krasnaya-polyana-map.jpg'),
+    sourcePreview: path.join(IMAGES_DIR, 'krasnaya-polyana-map-preview.jpg')
+  },
+  rosaKhutor: {
+    filename: 'rosa-khutor-map.jpg',
+    source: path.join(IMAGES_DIR, 'rosa-khutor-map.jpg'),
+    sourcePreview: path.join(IMAGES_DIR, 'rosa-khutor-map-preview.jpg')
+  },
+  gazprom: {
+    filename: 'gazprom-map.jpg',
+    source: path.join(IMAGES_DIR, 'gazprom-map.jpg'),
+    sourcePreview: path.join(IMAGES_DIR, 'gazprom-map-preview.jpg')
+  }
+};
+
+async function downloadMap(ctx, filename) {
+  let source = String;
+
+  switch (filename) {
+    case TRAIL_MAPS.mainMap.filename:
+      source = TRAIL_MAPS.mainMap.source;
+      break;
+    case TRAIL_MAPS.krasnayaPolyana.filename:
+      source = TRAIL_MAPS.krasnayaPolyana.source;
+      break;
+    case TRAIL_MAPS.rosaKhutor.filename:
+      source = TRAIL_MAPS.rosaKhutor.source;
+      break;
+    case TRAIL_MAPS.gazprom.filename:
+      source = TRAIL_MAPS.gazprom.source;
+      break;
+  }
+
+  try {
+    await ctx.replyWithDocument({ source, filename });
+  } catch (error) {
+    logger.debug(ctx, error);
+  }
+}
+
+module.exports = { downloadMap, TRAIL_MAPS };
